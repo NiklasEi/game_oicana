c25ffb17854172a54336c7f0f24b1706926d2d26
Niklas Eicker <git@nikl.me>
2021-01-17 18:27:23 +0100

    Replace lost text with retry button (WIP)

--- crates/oicana_plugin/src/ui.rs
+++ crates/oicana_plugin/src/ui.rs
@@ -4,16 +4,31 @@ pub struct UiPlugin;
 
 impl Plugin for UiPlugin {
     fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(GameState {
-            health: 20,
-            score: 0,
-            enemy_health: 10,
-        })
-        .add_startup_system(init_life.system())
-        .add_system(update_game_state.system())
-        .add_system(show_lost.system());
+        app.add_resource(GameState::default())
+            .init_resource::<ButtonMaterials>()
+            .add_startup_system(init_life.system())
+            .add_system(update_game_state.system())
+            .add_system(retry_system.system())
+            .add_system(click_retry_button.system());
     }
 }
+
+struct ButtonMaterials {
+    normal: Handle<ColorMaterial>,
+    hovered: Handle<ColorMaterial>,
+}
+
+impl FromResources for ButtonMaterials {
+    fn from_resources(resources: &Resources) -> Self {
+        let mut materials = resources.get_mut::<Assets<ColorMaterial>>().unwrap();
+        ButtonMaterials {
+            normal: materials.add(Color::rgb(0.15, 0.15, 0.15).into()),
+            hovered: materials.add(Color::rgb(0.25, 0.25, 0.25).into()),
+        }
+    }
+}
+
+struct RetryButton;
 
 struct HealthText;
 
@@ -23,6 +38,16 @@ pub struct GameState {
     pub health: usize,
     pub score: usize,
     pub enemy_health: i32,
+}
+
+impl Default for GameState {
+    fn default() -> Self {
+        GameState {
+            health: 20,
+            score: 0,
+            enemy_health: 1,
+        }
+    }
 }
 
 fn init_life(
@@ -103,55 +128,79 @@ fn init_life(
 
 fn update_game_state(
     game_state: ChangedRes<GameState>,
-    mut health_query: Query<(&mut Text, &HealthText)>,
-    mut score_query: Query<(&mut Text, &ScoreText)>,
+    mut health_query: Query<&mut Text, With<HealthText>>,
+    mut score_query: Query<&mut Text, With<ScoreText>>,
 ) {
-    for (mut text, _tag) in health_query.iter_mut() {
+    for mut text in health_query.iter_mut() {
         text.value = format!("Health: {}", game_state.health);
     }
-    for (mut text, _tag) in score_query.iter_mut() {
+    for mut text in score_query.iter_mut() {
         text.value = format!("Score: {}", game_state.score);
     }
 }
 
-fn show_lost(
+fn retry_system(
     commands: &mut Commands,
     asset_server: ResMut<AssetServer>,
-    health: ChangedRes<GameState>,
-    mut color_materials: ResMut<Assets<ColorMaterial>>,
+    game_state: ChangedRes<GameState>,
+    button_materials: Res<ButtonMaterials>,
 ) {
-    if health.health < 1 {
-        let font = asset_server.load("fonts/FiraSans-Bold.ttf");
-        let material = color_materials.add(Color::WHITE.into());
+    if game_state.health < 1 {
+        println!("spawning retry button");
         commands
-            .spawn(CameraUiBundle::default())
-            // root node
-            .spawn(NodeBundle {
+            .spawn(ButtonBundle {
                 style: Style {
-                    position_type: PositionType::Absolute,
-                    position: Rect {
-                        left: Val::Px(170.),
-                        top: Val::Px(200.),
-                        ..Default::default()
-                    },
+                    size: Size::new(Val::Px(150.0), Val::Px(65.0)),
+                    margin: Rect::all(Val::Auto),
+                    justify_content: JustifyContent::Center,
+                    align_items: AlignItems::Center,
                     ..Default::default()
                 },
-                material,
+                material: button_materials.normal.clone(),
                 ..Default::default()
             })
+            .with(RetryButton)
             .with_children(|parent| {
                 parent.spawn(TextBundle {
                     text: Text {
-                        value: "You lost! Restart to try again ;)".to_owned(),
-                        font,
+                        value: "Restart".to_string(),
+                        font: asset_server.load("fonts/FiraSans-Bold.ttf"),
                         style: TextStyle {
                             font_size: 40.0,
-                            color: Color::rgb(0.6, 0.6, 0.6),
+                            color: Color::rgb(0.9, 0.9, 0.9),
                             ..Default::default()
                         },
                     },
                     ..Default::default()
                 });
             });
+    }
+}
+
+fn click_retry_button(
+    commands: &mut Commands,
+    button_materials: Res<ButtonMaterials>,
+    mut game_state: ResMut<GameState>,
+    mut interaction_query: Query<
+        (Entity, &Interaction, &mut Handle<ColorMaterial>, &Children),
+        (Mutated<Interaction>, With<Button>),
+    >,
+    text_query: Query<Entity, With<Text>>,
+) {
+    for (button, interaction, mut material, children) in interaction_query.iter_mut() {
+        let text = text_query.get(children[0]).unwrap();
+        match *interaction {
+            Interaction::Clicked => {
+                *game_state = GameState::default();
+                commands.despawn(button);
+                commands.despawn(text);
+            }
+            Interaction::Hovered => {
+                *material = button_materials.hovered.clone();
+            }
+            Interaction::None => {
+                *material = button_materials.normal.clone();
+            }
+        }
     }
 }
