166ae8c2f84b5d8b78e79e06d4d95680c4395baa
Niklas Eicker <git@nikl.me>
2021-06-18 20:03:17 +0200

    Play background audio in the menu

--- oicana_plugin/src/audio.rs
+++ oicana_plugin/src/audio.rs
@@ -3,14 +3,14 @@ use crate::loading::AudioAssets;
 use crate::towers::TowerShot;
 use crate::AppState;
 use bevy::prelude::*;
-use bevy_kira_audio::{Audio, AudioPlugin};
+use bevy_kira_audio::{Audio, AudioChannel, AudioPlugin};
 
 pub struct InternalAudioPlugin;
 
 impl Plugin for InternalAudioPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_plugin(AudioPlugin)
-            .add_system_set(SystemSet::on_enter(AppState::InGame).with_system(start_audio.system()))
+            .add_system_set(SystemSet::on_enter(AppState::Menu).with_system(start_audio.system()))
             .add_system_set(
                 SystemSet::on_update(AppState::InGame)
                     .with_system(tower_shots.system())
@@ -21,8 +21,10 @@ impl Plugin for InternalAudioPlugin {
 }
 
 fn start_audio(audio_assets: Res<AudioAssets>, audio: Res<Audio>) {
+    let background_channel = AudioChannel::new("background".to_owned());
     audio.set_volume(0.15);
-    audio.play_looped(audio_assets.background.clone());
+    audio.set_volume_in_channel(0.15, &background_channel);
+    audio.play_looped_in_channel(audio_assets.background.clone(), &background_channel);
 }
 
 fn stop_audio(audio: Res<Audio>) {
